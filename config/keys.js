d6ffd4128a0a20172d737457e5af8585ca3cb89d
Martin Funk <Martin@MartinFunk.de>
2019-12-01 14:53:58 +0100

    wip

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb://mongo:27017/ws-auth",
-    secretOrKey: "secret"
-};
-  
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys');
+const config = require('config');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 const User = require('../../models/User');
@@ -43,50 +43,60 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   const errorMsg = 'Authentication failed';
 
   const { errors, isValid } = validateLoginInput(req.body);
   if (!isValid) {
     return res.status(400).json(errors);
   }
-  const email = req.body.email;
-  const password = req.body.password;
-  User.findOne({
-    email
-  }).then(user => {
+
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+      email
+    });
+
     if (!user) {
       return res.status(404).json({
         authFailure: errorMsg
       });
     }
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        const payload = {
-          id: user.id,
-          name: user.name,
-          myPayload: 'Dieser Token ist nur zum Lesen gültig.'
-        };
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 180 // seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: 'Bearer ' + token
-            });
-          }
-        );
-      } else {
-        return res.status(400).json({
-          authFailure: errorMsg
-        });
-      }
-    });
-  });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch)
+    console.log(password)
+    console.log(user.password)
+    if (isMatch) {
+      const payload = {
+        id: user.id,
+        name: user.name,
+        myPayload: 'Dieser Token ist nur zum Lesen gültig.'
+      };
+      jwt.sign(
+        payload,
+        config.get('secretOrKey'),
+        {
+          expiresIn: 180 // seconds
+        },
+        (err, token) => {
+          return res.json({
+            success: true,
+            token: 'Bearer ' + token
+          });
+        }
+      );
+    } else {
+      return res.status(400).json({
+        authFailure: errorMsg
+      });
+    }
+  } catch {
+    console.error(err.message);
+    return res.status(500).send('server errror');
+  }
 });
 
 module.exports = router;
