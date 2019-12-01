a344cc7537ea417f78f8b6bcd6ada59e0ec7b9a9
Martin Funk <Martin@MartinFunk.de>
2019-12-01 13:20:32 +0100

    wip

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 var cors = require('cors');
-const mongoose = require('mongoose');
-const dbURI = require('./config/keys').mongoURI;
-const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 
 const passport = require('passport');
@@ -13,20 +10,11 @@ const app = express();
 connectDB();
 
 app.use(cors());
-// Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Passport middleware
 app.use(passport.initialize());
-// Passport config
 require('./config/passport')(passport);
-// Routes
 app.use('/api/users', users);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
